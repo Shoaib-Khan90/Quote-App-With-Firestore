2f2332eca40ebec7d07dfb7828285ea0ac66f171
Shoaib Khan <shoaib7878687@gmail.com>
2026-03-04 14:36:38 +0500

    updated changes

--- Firebase.js
+++ Firebase.js
@@ -21,15 +21,15 @@ import {
   serverTimestamp
 } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-firestore.js";
 
-  const firebaseConfig = {
-  apiKey: "AIzaSyC5l8oQJM9N4MSSW3iNlwhkRiZbtOt6luw",
-  authDomain: "a-new-ui.firebaseapp.com",
-  projectId: "a-new-ui",
-  storageBucket: "a-new-ui.appspot.com",
-  messagingSenderId: "316943913089",
-  appId: "1:316943913089:web:68ce81bd2aac9f9167fd80",
-  measurementId: "G-B1R5P8PLPV"
-};
+ const firebaseConfig = {
+    apiKey: "AIzaSyBIBVVl1-qah5qPAns6ULaO8QmjfcnugiA",
+    authDomain: "ecommerce-7f1e2.firebaseapp.com",
+    projectId: "ecommerce-7f1e2",
+    storageBucket: "ecommerce-7f1e2.firebasestorage.app",
+    messagingSenderId: "1090647740235",
+    appId: "1:1090647740235:web:97a023e4b6638f1c7022ce",
+    measurementId: "G-JE0WY3T385"
+  };
 
  export {
   firebaseConfig,
